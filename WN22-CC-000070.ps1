f47223f2401748889b494c1b3f991e5bf0f021d7
Reginald-D <152654163+Reginald-D@users.noreply.github.com>
2026-01-21 11:36:23 -0500

    Update WN22-CC-000070.ps1

--- WN22-CC-000070.ps1
+++ WN22-CC-000070.ps1
@@ -1,18 +1,17 @@
 ﻿<#
-.SYNOPSIS
+SYNOPSIS
 Disables insecure guest logons to SMB servers to prevent unauthenticated access.
 
-.AUTHOR
-Reggie Ruler
+AUTHOR: Reginald D
 
-.STIG
+STIG
 ID: WN22-CC-000070
 SRG: SRG-OS-000480-GPOS-00227
 Severity: Medium (CAT II)
 CCI: CCI-000366
 Vulnerability ID: V-254339
 
-.VULNERABILITY DISCUSSION
+VULNERABILITY DISCUSSION
 Insecure guest logons permit unauthenticated access to shared folders over SMB.
 This increases the risk of unauthorized data access and lateral movement.
 Disabling this setting enforces authentication for all SMB connections.
@@ -28,4 +27,5 @@ if((Get-ItemProperty -Path $Path -Name $Name).AllowInsecureGuestAuth -eq 0){
     Write-Host "WN22-CC-000070 compliant: Insecure SMB guest logons disabled."
 }else{
     Write-Host "WN22-CC-000070 non-compliant: remediation failed."
+
 }
