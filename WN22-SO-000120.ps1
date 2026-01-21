b4191cbdbc97bde8a4b6510056f552bce8449232
Reginald-D <152654163+Reginald-D@users.noreply.github.com>
2026-01-21 11:40:35 -0500

    Add files via upload

---
+++ WN22-SO-000120.ps1
@@ -0,0 +1,27 @@
+﻿<#
+SYNOPSIS:Configures the machine inactivity limit to lock the system after 15 minutes of inactivity.
+
+
+AUTHOR: Reginald D
+STIGID: WN22-SO-000120
+SRG: SRG-OS-000028-GPOS-00009
+Severity: Medium (CAT II)
+CCI: CCI-000056, CCI-000057, CCI-000060
+Vulnerability ID: V-254456
+
+
+.VULNERABILITY DISCUSSION
+Unattended systems are vulnerable to unauthorized use. Enforcing a machine inactivity
+limit ensures the system locks automatically after a defined period, protecting
+sensitive data from unauthorized physical access.
+#>
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$regName = "InactivityTimeoutSecs"
+$timeout = 900
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name $regName -Type DWord -Value $timeout
