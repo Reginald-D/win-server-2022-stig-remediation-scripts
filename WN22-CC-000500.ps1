03b8ef1c7336e83ed5a611a0aac670a7abbba83b
Reginald-D <152654163+Reginald-D@users.noreply.github.com>
2026-01-21 11:36:42 -0500

    Update WN22-CC-000500.ps1

--- WN22-CC-000500.ps1
+++ WN22-CC-000500.ps1
@@ -4,13 +4,9 @@ SYNOPSIS: Disables WinRM Basic authentication.
 AUTHOR: Reginald D
 
 STIG ID:WN22-CC-000500
-
 SRG: SRG-OS-000125-GPOS-00065
-
 SEVERITY: High (CAT I)
-
 CCI: CCI-000877
-
 VULNERABILITY ID: V-254381
 
 VULNERABILITY DISCUSSION
@@ -30,3 +26,4 @@ if ((Get-ItemProperty $Path).AllowBasic -eq 0) {
 } else {
     Write-Host "WinRM Basic authentication remediation failed."
 }
+
